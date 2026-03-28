fda9ad9176bccebe0d49c00b2c68571d9eca3d97
Chris Antonie Pieterse <chrisantoniep@gail.com>
2026-03-28 21:01:14 +0100

    Remove scaffolding

--- public/views/contact.js
+++
@@ -1,7 +0,0 @@
-export default async function Contact() {
-  console.log("Rendering Contact view");
-  return `
-    <h1>Contact Me</h1>
-    <p>You can reach me at <a href="mailto:chrisantoniep@gmail.com">chrisantoniep@gmail.com</a></p>
-  `;
-}
--- public/views/home.js
+++
@@ -1,7 +0,0 @@
-export default async function Home() {
-  console.log("Rendering Home view");
-  return `
-    <h1>Chris Pieterse</h1>
-    <p>Software Engineer & Technical Consultant</p>
-  `;
-}
